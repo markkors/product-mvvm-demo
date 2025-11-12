fe6e2cfcffd9fbf842c18aa6efad2806f0320671
Mark Kors <m.kors@rocmn.nl>
2025-11-12 15:16:52 +0100

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_DEMO.Models;
+
+namespace MVVM_DEMO.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        
+    
+        private ObservableCollection<Product> _products;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // constructor
+
+        public MainViewModel()
+        {
+            _products = new ObservableCollection<Product>();
+            LoadData();
+        }
+
+        // read data
+        private  void LoadData()
+        {
+            _products.Add(new Product { ProductName = "Product 1", Price = 10.0 });
+            _products.Add(new Product { ProductName = "Product 2", Price = 20.0 });
+            _products.Add(new Product { ProductName = "Product 3", Price = 30.0 });
+            Products = _products;
+        }
+
+
+        // properties
+        public ObservableCollection<Product> Products { get; set; }
+
+        public string productName { get; set; }
+        public int productPrice { get; set; }
+
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using MVVM_DEMO.Models;
+using MVVM_DEMO.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVM_DEMO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel viewModel = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+            comboBox.SelectionChanged += ComboBox_SelectionChanged;
+            // initial selection
+            if (comboBox.Items.Count > 0)
+            {
+                comboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // display selected product details
+            if (comboBox.SelectedItem != null)
+            {
+                viewModel.productName= ((Product)comboBox.SelectedItem).ProductName;
+                viewModel.productPrice = (int)((Product)comboBox.SelectedItem).Price;
+                viewModel.OnPropertyChanged("productName");
+                viewModel.OnPropertyChanged("productPrice");
+            }
+
+        }
+
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            // toevoegen van een product in het viewmodel
+            Product p = new Product();
+            p.ProductName = $"Product {viewModel.Products.Count + 1}";
+            // generate a random price between 10 and 100
+            Random rand = new Random();
+            p.Price = rand.Next(10, 100);
+            viewModel.Products.Add(p);
+        }
+    }
+}
